c095b33f2bdeb41da8aac1d8a74dc471f2c08ee3
Kristina Moiseeva <kristina@192.168.1.4>
2024-03-14 22:54:19 +0300

    fix src/cli.js and  bin/brain-games.js

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const text = () => {
+const welcome = () => {
     console.log("brain-games");
     console.log("Welcome to the Brain Games!");
     const name = readlineSync.question('May I have your name? ')
-    console.log(`Hello, ${name}`)
+    console.log(`Hello, ${name}!`)
 };  
 
-export { text }; 
+export { welcome }; 
